f6e4d3b3ae2a7184f9eece6840ec94a8b63a9420
MohammadAmin Sabbaghian <sabbaghian@arsh.co>
2016-01-29 10:25:26 +0330

    تکمیل کرال

---
+++ crawler/crawler/queue.py
@@ -0,0 +1,12 @@
+import Queue
+
+
+class SetQueue(Queue.Queue):
+    def _init(self, maxsize):
+        self.queue = set()
+
+    def _put(self, item):
+        self.queue.add(item)
+
+    def _get(self):
+        return self.queue.pop()
